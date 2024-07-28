3a239fff3e65303f1be33caa71863a601eedcb77
LeAnne B. <branchleannelmb@gmail.com>
2024-07-28 11:27:44 -0400

    inputs beginning of checking the game status logic

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ const cellDivs = document.querySelectorAll(".game-cell");
 let gameIsLive = true;
 let xIsNext = true;
 
+//functions
+
+const checkGameStatus = () => {
+  const topLeft = cellDivs[0].classList[2];
+  const topMiddle = cellDivs[1].classList[2];
+  const topRight = cellDivs[2].classList[2];
+  const middleLeft = cellDivs[3].classList[2];
+  const middleMiddle = cellDivs[4].classList[2];
+  const middleRight = cellDivs[5].classList[2];
+  const bottomLeft = cellDivs[6].classList[2];
+  const bottomMiddle = cellDivs[7].classList[2];
+  const bottomRight = cellDivs[8].classList[2];
+};
+
 const handleReset = (e) => {
   console.log(e);
 };
@@ -25,9 +39,11 @@ const handleCellClick = (e) => {
   if (xIsNext) {
     //console.log(e.target);
     classList.add("x");
+    checkGameStatus();
     xIsNext = !xIsNext;
   } else {
     classList.add("o");
+    checkGameStatus();
     xIsNext = !xIsNext;
   }
 };
